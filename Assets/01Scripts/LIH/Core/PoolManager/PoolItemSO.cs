cf479f4e563ec6e2dcbc01f8d1c44ad1d37e441f
inhae0519 <lsh266409@gmail.com>
2024-12-18 00:22:53 +0900

    fix : project

--- Assets/01Scripts/LIH/Core/PoolManager/PoolItemSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "SO/Pool/Item")]
-public class PoolItemSO : ScriptableObject
-{
-    public PoolType poolType;
-    public GameObject prefab;
-    public int initCount;
-}
