909e5e9fc6350e8817a0ef17cd48d914e9da34db
inhae0519 <lsh266409@gmail.com>
2024-12-19 21:24:10 +0900

    player

--- Assets/01Scripts/BAS/ParticleEmisiionHandler.cs
+++ Assets/01Scripts/BAS/ParticleEmisiionHandler.cs
@@ -8,7 +8,6 @@ public class ParticleEmisiionHandler : MonoBehaviour
     public void ChangeEmission(float aa)
     {
         ParticleSystem.EmissionModule emission = _particleSystem.emission;
-
         emission.rateOverTime = aa;
     }
 }
--- Assets/01Scripts/LIH/Player/PlayerWeapon/Weapon.cs
+++ Assets/01Scripts/LIH/Player/PlayerWeapon/Weapon.cs
@@ -1,11 +1,16 @@
+using System;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
 public abstract class Weapon : MonoBehaviour
 {
     [SerializeField] private PlayerMuzzleEffect _playerMuzzleEffect;
+    [SerializeField] private ParticleEmisiionHandler _emisiion;
     [SerializeField] protected GameEventChannelSO _spawnChannel;
     [SerializeField] protected PlayerBullet _bullet;
+
+    [SerializeField] private float _particleMaxSize;
     [SerializeField] private float _recoilTime;
     [SerializeField] private float _recoilXValue;
 
@@ -16,6 +21,8 @@ public abstract class Weapon : MonoBehaviour
     protected Transform _visual;
     protected Transform _fireTrm;
     protected Player _player;
+
+    private ParticleEmisiionHandler _currentEmisiion;
     
     private void Awake()
     {
@@ -23,11 +30,21 @@ public abstract class Weapon : MonoBehaviour
         _fireTrm = transform.Find("FirePos");
     }
 
-    public void SetOwner(Player player) => _player = player;
+    public void SetOwner(Player player)
+    {
+        _player = player;
+        
+        _player.GetPlayerCompo<PlayerWeaponController>().resetEvent.AddListener(HandleEmmisionReset);
+    }
 
+    private void OnDestroy()
+    {
+        _player.GetPlayerCompo<PlayerWeaponController>().resetEvent.RemoveListener(HandleEmmisionReset);
+    }
+    
     public virtual void Charging(float chargingTime ,float chargingValue)
     {
-//        Debug.Log(chargingValue);
+        
     }
 
     public virtual void Fire(float power)
@@ -55,5 +72,14 @@ public abstract class Weapon : MonoBehaviour
         seq.Append(_visual.DOLocalMoveX(_recoilXValue, _recoilTime));
         seq.Append(_visual.DOLocalMoveX(_defaultPos.x, _recoilTime))
             .OnComplete(() => _visual.localPosition = _defaultPos);
+    }
+    
+    private void HandleEmmisionReset()
+    {
+        if(_currentEmisiion == null)
+            return;
+
+        _currentEmisiion.GetComponentsInChildren<ParticleSystem>().ToList().ForEach(x 
+            => x.loop = false);
     }
 }
