76b911e37d9e0fbce67e942eb8407924b9461ed9
chunma1126 <123716103+chunma1126@users.noreply.github.com>
2025-03-18 15:36:39 +0900

    FIx Bugs

--- Assets/01Scripts/LIH/UI/MenuUI/OptionUI.cs
+++ Assets/01Scripts/LIH/UI/MenuUI/OptionUI.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 public class OptionUI : MonoBehaviour
 {
+    public FadeIn FadeIn;
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void TransitionLobby()
+    {
+        FadeIn.FadeStart("LobbyScene");   
     }
 }
